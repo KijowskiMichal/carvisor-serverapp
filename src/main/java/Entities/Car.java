c78d75a161b379d2fc66ff69ddc7424004640535
Bartosz <skonieczka.b@gmail.com>
2021-04-01 22:54:14 +0200

    Car Entity added

---
+++ src/main/java/Entities/Car.java
@@ -0,0 +1,46 @@
+package Entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.LocalDate;
+
+
+@Entity
+public class Car {
+    /**
+     * Identification number
+     */
+    @Id
+    @GeneratedValue
+    int id;
+    /**
+     * Actual license plate
+     */
+    String licensePlate;
+    /**
+     * Car Brand
+     */
+    String brand;
+    /**
+     * Car model
+     */
+    String model;
+    /**
+     * Car production date
+     */
+    LocalDate productionDate;
+    /**
+     * Date of incorporation of vehicles into the fleet
+     */
+    LocalDate inCompanyDate;
+
+    public Car(int id, String licensePlate, String brand, String model, LocalDate productionDate, LocalDate inCompanyDate) {
+        this.id = id;
+        this.licensePlate = licensePlate;
+        this.brand = brand;
+        this.model = model;
+        this.productionDate = productionDate;
+        this.inCompanyDate = inCompanyDate;
+    }
+}
