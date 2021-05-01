5a95db23a88fad918ed38633384b83a8844fbd0a
Hosurgo <michal@kronmar.eu>
2021-05-01 11:05:28 +0200

    UserREST.changePassword added

--- src/main/java/RestPackage/UsersREST.java
+++ src/main/java/RestPackage/UsersREST.java
@@ -4,12 +4,15 @@ import Entities.Track;
 import Entities.User;
 import Entities.UserPrivileges;
 import HibernatePackage.HibernateRequests;
+import org.apache.commons.codec.digest.DigestUtils;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -27,26 +30,25 @@ import java.util.List;
  */
 @RestController
 @RequestMapping("/users")
-public class UsersREST
-{
+public class UsersREST {
     /**
+     * @param request  Object of HttpServletRequest represents our request;
+     * @param page     Page of users list. Parameter associated with pageSize.
+     * @param pageSize Number of record we want to get
+     * @param regex    Part of name or surname we want to display
      * @return Returns the contents of the page that contains a list of users in the JSON format.
      *
      * WebMethod which returns a list of users.
      */
     @RequestMapping(value = "/list/{page}/{pagesize}/{regex}/", produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.GET)
-    public ResponseEntity<String> list(HttpServletRequest request, @PathVariable("page") int page, @PathVariable("pagesize") int pageSize, @PathVariable("regex") String regex)
-    {
+    public ResponseEntity<String> list(HttpServletRequest request, @PathVariable("page") int page, @PathVariable("pagesize") int pageSize, @PathVariable("regex") String regex) {
         // authorization
-        if (request.getSession().getAttribute("user")==null)
-        {
+        if (request.getSession().getAttribute("user") == null) {
             Initializer.getLogger().info("UserREST.list cannot list user's (session not found)");
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("[]");
-        }
-        else if ((((User)request.getSession().getAttribute("user")).getUserPrivileges()!=UserPrivileges.ADMINISTRATOR) && (((User)request.getSession().getAttribute("user")).getUserPrivileges()!=UserPrivileges.MODERATOR))
-        {
-            Initializer.getLogger().info("UserREST.list cannot list user's because rbac (user: "+((User)request.getSession().getAttribute("user")).getNick()+")");
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("[]");
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("");
+        } else if ((((User) request.getSession().getAttribute("user")).getUserPrivileges() != UserPrivileges.ADMINISTRATOR) && (((User) request.getSession().getAttribute("user")).getUserPrivileges() != UserPrivileges.MODERATOR)) {
+            Initializer.getLogger().info("UserREST.list cannot list user's because rbac (user: " + ((User) request.getSession().getAttribute("user")).getNick() + ")");
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("");
         }
         //listing
         List<Object> users = new ArrayList<>();
@@ -56,40 +58,37 @@ public class UsersREST
         try {
             if (regex.equals("$")) regex = "";
             tx = session.beginTransaction();
-            String countQ = "Select count (u.id) from User u WHERE u.name  like '%"+regex+"%' OR u.surname  like '%"+regex+"%' ";
+            String countQ = "Select count (u.id) from User u WHERE u.name  like '%" + regex + "%' OR u.surname  like '%" + regex + "%' ";
             Query countQuery = session.createQuery(countQ);
             Long countResults = (Long) countQuery.uniqueResult();
-            lastPageNumber = (int) (Math.ceil(countResults / (double)pageSize));
+            lastPageNumber = (int) (Math.ceil(countResults / (double) pageSize));
 
-            Query selectQuery = session.createQuery("SELECT u FROM User u WHERE u.name  like '%"+regex+"%' OR u.surname  like '%"+regex+"%' ");
+            Query selectQuery = session.createQuery("SELECT u FROM User u WHERE u.name  like '%" + regex + "%' OR u.surname  like '%" + regex + "%' ");
             selectQuery.setFirstResult((page - 1) * pageSize);
             selectQuery.setMaxResults(pageSize);
             users = selectQuery.list();
             tx.commit();
             session.close();
         } catch (HibernateException e) {
-            if (tx!=null) tx.rollback();
+            if (tx != null) tx.rollback();
             session.close();
             e.printStackTrace();
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("");
         }
         JSONObject jsonOut = new JSONObject();
         JSONArray jsonArray = new JSONArray();
-        for (Object tmp : users)
-        {
+        for (Object tmp : users) {
             JSONObject jsonObject = new JSONObject();
-            jsonObject.put("nick", ((User)tmp).getNick());
-            jsonObject.put("name", ((User)tmp).getName());
-            jsonObject.put("surname", ((User)tmp).getSurname());
-            Track tmpTrack = ((User)tmp).getTrack();
-            if (tmpTrack==null)
-            {
+            jsonObject.put("id", ((User) tmp).getId());
+            jsonObject.put("nick", ((User) tmp).getNick());
+            jsonObject.put("name", ((User) tmp).getName());
+            jsonObject.put("surname", ((User) tmp).getSurname());
+            Track tmpTrack = ((User) tmp).getTrack();
+            if (tmpTrack == null) {
                 jsonObject.put("status", "inactive");
                 jsonObject.put("startTime", "");
                 jsonObject.put("finishTime", "");
-            }
-            else
-            {
+            } else {
                 jsonObject.put("status", "active");
                 jsonObject.put("startTime", tmpTrack.getStartTime().toString());
                 jsonObject.put("finishTime", "");
@@ -100,7 +99,45 @@ public class UsersREST
         jsonOut.put("page", page);
         jsonOut.put("pageMax", lastPageNumber);
         jsonOut.put("listOfUsers", jsonArray);
-        Initializer.getLogger().info("UsersREST.list returns list of users (user: "+((User)request.getSession().getAttribute("user")).getNick()+")");
+        Initializer.getLogger().info("UsersREST.list returns list of users (user: " + ((User) request.getSession().getAttribute("user")).getNick() + ")");
         return ResponseEntity.status(HttpStatus.OK).body(jsonOut.toString());
+    }
+
+    /**
+     * @param request  Object of HttpServletRequest represents our request;
+     * @param httpEntity Object of HttpEntity represents content of our request;
+     * @return HttpStatus.UNAUTHORIZED if session not found, HttpStatus.OK if all is ok, BAD_REQUEST if json haven't required data or password don't match
+     */
+    @RequestMapping(value = "/changePassword", method = RequestMethod.POST)
+    public ResponseEntity changePassword(HttpServletRequest request, HttpEntity<String> httpEntity) {
+        // authorization
+        if (request.getSession().getAttribute("user") == null) {
+            Initializer.getLogger().info("UserREST.changePassword cannot work (session not found)");
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("");
+        }
+        try {
+            JSONObject inJSON = new JSONObject(httpEntity.getBody());
+            if (inJSON.getString("password1").equals(inJSON.getString("password2"))) {
+                Session session = HibernatePackage.EntityFactory.getFactory().openSession();
+                Transaction tx = null;
+                try {
+                    tx = session.beginTransaction();
+                    User user = (User) request.getSession().getAttribute("user");
+                    user.setPassword(DigestUtils.sha256Hex(inJSON.getString("password1")));
+                    request.getSession().setAttribute("user", user);
+                    session.update(user);
+                    tx.commit();
+                    session.close();
+                    return ResponseEntity.status(HttpStatus.OK).body("");
+                } catch (HibernateException e) {
+                    if (tx != null) tx.rollback();
+                    session.close();
+                    e.printStackTrace();
+                    return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body("");
+                }
+            } else return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("");
+        }
     }
 }
