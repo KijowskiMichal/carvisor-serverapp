238024051140a3f272b7d715e85bd5ad91f5e3ce
Bartosz Skonieczka <skonieczka.b@gmail.com>
2021-12-01 20:54:38 +0100

    + FIX

--- src/test/java/com/inz/carvisor/service/CalendarServiceTest.java
+++ src/test/java/com/inz/carvisor/service/CalendarServiceTest.java
@@ -1,20 +1,40 @@
 package com.inz.carvisor.service;
 
+import com.inz.carvisor.dao.CalendarDaoJdbc;
+import com.inz.carvisor.dao.CarDaoJdbc;
 import com.inz.carvisor.entities.builders.EventBuilder;
+import com.inz.carvisor.entities.model.Car;
 import com.inz.carvisor.entities.model.Event;
+import com.inz.carvisor.otherclasses.Initializer;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.ContextConfiguration;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@WebMvcTest(CalendarDaoJdbc.class)
+@ContextConfiguration(classes = {Initializer.class})
 class CalendarServiceTest {
 
     private static long TIMESTAMP = 1638385702;
 
+    @Autowired
+    private CalendarDaoJdbc calendarDaoJdbc;
+
+    @AfterEach
+    void clearDatabase() {
+        calendarDaoJdbc.getAll().stream().map(Event::getId).forEach(calendarDaoJdbc::delete);
+    }
+
     @Test
     void add() {
-
+        mockEventList().forEach(calendarDaoJdbc::save);
+        List<Event> all = calendarDaoJdbc.getAll();
+        assertEquals(mockEventList().size(),all.size());
     }
 
     @Test
