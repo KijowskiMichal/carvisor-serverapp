555d924056600f3ee1b5bee98a64f5b7cb3511f9
Michał <michal@kronmar.eu>
2022-01-11 20:56:59 +0100

    1

--- src/main/java/com/inz/carvisor/controller/RankingController.java
+++ src/main/java/com/inz/carvisor/controller/RankingController.java
@@ -63,8 +63,8 @@ public class RankingController {
         int maxPage = getUserTracks.size() / pagesize + 1;
 
         int from = ( page - 1 ) * pagesize;
-        int to = Math.min(getUserTracks.size(), page * maxPage - 1);
-        JSONObject jsonObject = toJson(userToCheck, safetyPointsRankingPosition, ecoPointsRankingPosition, getUserTracks)
+        int to = Math.min(getUserTracks.size(), page * pagesize);
+        JSONObject jsonObject = toJson(userToCheck, safetyPointsRankingPosition, ecoPointsRankingPosition, getUserTracks.subList(from, to))
                 .put(AttributeKey.CommonKey.PAGE, page)
                 .put(AttributeKey.CommonKey.PAGE_MAX, maxPage);
         return DefaultResponse.ok(jsonObject.toString());
