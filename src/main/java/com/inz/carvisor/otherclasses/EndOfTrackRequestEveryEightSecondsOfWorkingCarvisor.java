94686fefafb96ab1d16590aca6616abc03e01c98
Michał <michal@kronmar.eu>
2022-01-14 00:07:22 +0100

    [fix] poprawa interwałowego kończenia tras

--- src/main/java/com/inz/carvisor/otherclasses/EndOfTrackRequestEveryEightSecondsOfWorkingCarvisor.java
+++ src/main/java/com/inz/carvisor/otherclasses/EndOfTrackRequestEveryEightSecondsOfWorkingCarvisor.java
@@ -1,15 +1,15 @@
 package com.inz.carvisor.otherclasses;
 
 import com.inz.carvisor.service.TrackService;
-import org.springframework.scheduling.annotation.Async;
-import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
-@EnableAsync
+@Component
 public class EndOfTrackRequestEveryEightSecondsOfWorkingCarvisor {
+    @Autowired
     private TrackService trackService;
 
-    @Async
     @Scheduled(fixedRate = 8000)
     public void endOfTrackRequestEveryEightSecondsOfWorkingCarvisor() throws InterruptedException {
         trackService.endOfTrack(null, null);
